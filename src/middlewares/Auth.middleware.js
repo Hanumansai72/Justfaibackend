8c24adcbac4978a84f7259694625a47ab5eb6e58
User <user@example.com>
2026-03-09 10:22:38 +0530

    added env

--- src/middlewares/Auth.middleware.js
+++ src/middlewares/Auth.middleware.js
@@ -10,7 +10,7 @@ function verifyToken(req, res, next) {
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET||"JUSTFAI");
     req.user = decoded;
     next();
   } catch (err) {
